f04d93da7c94d057579331d6fb46859d361a5eae
Gabriel Giro Resende <gabriel.resende@westwing.com.br>
2020-08-18 20:57:27 -0300

    Modularizando o menu e criada a pagina da rede
    - Modularizamos o menu para extrair em um único arquivo css todo seu
    conteúdo (assim vamos poder usar em outras paginas também)
    - Adicionadas algumas novas imagens, úteis a página da rede
    - Finalizada a pagina interna da rede, já com a versão mobile ajustada

---
+++ Portal/assets/js/menu.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    var navbar = document.getElementsByClassName("navbar")[0];
+    navbar.style.left = "0";
+}
+
+function closeMenu() {
+    var navbar = document.getElementsByClassName("navbar")[0];
+    navbar.style.left = "-80%";
+}
