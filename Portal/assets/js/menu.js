3037468ddd12172666d3918562e5822e9a008832
luizerko <luisvz@gmail.com>
2020-08-25 20:48:21 -0300

    Páginas revisadas e devidamente ajustadas. Primeira versão finalizada do site.

--- Portal/assets/js/menu.js
+++ Portal/assets/js/menu.js
@@ -7,3 +7,20 @@ function closeMenu() {
     var navbar = document.getElementsByClassName("navbar")[0];
     navbar.style.left = "-80%";
 }
+
+function onResize() {
+    var navbar = document.getElementsByClassName("navbar")[0];
+    navbar.style.transition = 'left 0s';
+    if(window.innerWidth >= 768) {
+        navbar.style.left = "0";
+    }
+    else{
+        navbar.style.left = "-80%";
+    }
+
+    setTimeout(function() {
+        navbar.style.transition = 'left 1s';
+    }, 500);
+}
+    
+window.addEventListener('resize', onResize);
